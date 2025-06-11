ad2313134e1914b8d0e30fd9c1f3c1d2c426cb30
ToratYosef <yoseftorat@gmail.com>
2025-06-11 12:06:48 -0400

    Add files via upload

---
+++ scripts/firebase-config.js
@@ -0,0 +1,10 @@
+// /scripts/firebase-config.js
+export const firebaseConfig = {
+  apiKey: "AIzaSyDfXnCZXN-URjdvFvVlWHLg4KOkSw7hvng",
+  authDomain: "torat-yosef.firebaseapp.com",
+  projectId: "torat-yosef",
+  storageBucket: "torat-yosef.appspot.com",
+  messagingSenderId: "1033400220494",
+  appId: "1:1033400220494:web:1437414bdad4439fd6bc1f",
+  measurementId: "G-331D9RHZWH"
+};
