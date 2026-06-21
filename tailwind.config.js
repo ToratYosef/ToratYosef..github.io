9e1a5f6086ab939227d0562ebc143e86b56a07fa
ToratYosef <yoseftorat@gmail.com>
2026-06-21 16:19:42 +0000

    Temp pre-rebase commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: [
+    './*.html',
+    './admin/**/*.html',
+    './thank-you/**/*.html',
+    './scripts/**/*.js'
+  ],
+  theme: {
+    extend: {}
+  },
+  plugins: []
+};
